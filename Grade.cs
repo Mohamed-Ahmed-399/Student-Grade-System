988d13d30c366fcd4a52e8b55cd3342a05033e3c
Mohamed-Ahmed-399 <ma2097706@gmail.com>
2026-08-28 06:51:26 +0200

    First Commit

---
+++ Grade.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Grade_System
+{
+    public class Grade
+    {
+        public void PrintGrade(string studentName, int grade)
+        {
+            Console.WriteLine($"Student: {studentName}, Grade: {grade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Student Grades System");
